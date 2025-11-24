a49c080ddfe80f87ce517f3293d17373056d10b4
Brett Blomberg <49074821+BrettBlomb@users.noreply.github.com>
2025-11-24 14:04:00 -0800

    Update test_mobile_web.py

--- tests/test_mobile_web.py
+++ tests/test_mobile_web.py
@@ -1,20 +1,31 @@
 from appium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def test_mobile_browser():
+    options = Options()
+    options.add_argument("--disable-notifications")
+    
     desired_caps = {
         "platformName": "Android",
         "automationName": "UiAutomator2",
-        "browserName": "Chrome",
         "deviceName": "Android Emulator",
+        "browserName": "Chrome",
     }
 
-    driver = webdriver.Remote("http://127.0.0.1:4723", desired_caps)
+    driver = webdriver.Remote(
+        "http://127.0.0.1:4723",
+        options=options,
+        desired_capabilities=desired_caps
+    )
+
+    wait = WebDriverWait(driver, 10)
 
     driver.get("https://the-internet.herokuapp.com/")
-    title = driver.title
-
-    assert "The Internet" in title
+    title_element = wait.until(EC.visibility_of_element_located((By.TAG_NAME, "h1")))
+    
+    assert "The Internet" in driver.title
 
     driver.quit()
